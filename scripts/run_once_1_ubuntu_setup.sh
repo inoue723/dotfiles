c02b5dfd8d06e32e4d651a203790d9d380a9fa07
inoue723 <omuni723@gmail.com>
2024-03-31 10:38:57 +0900

    scripts: add a package install script

--- scripts/run_once_1_ubuntu_setup.sh
+++ scripts/run_once_1_ubuntu_setup.sh
@@ -17,25 +17,6 @@ cargo install xremap --features gnome
 # 起動時にxremapを起動する
 systemctl --user enable $HOME/.local/share/chezmoi/xremap/systemd/xremap.service
 
-# Install Docker
-# ref. https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
-sudo apt-get install -y \
-    ca-certificates \
-    curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-# Use docker command without sudo
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
 ### gsettings
 # Dockの常時表示を無効化
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
