80dd27238b271ef3298a7ab57622c34e4ce86283
inoue723 <omuni723@gmail.com>
2024-04-04 21:08:53 +0900

    disable super + number shortcut

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -89,3 +89,6 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Ctrl>2'
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Ctrl>3']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Ctrl>4']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Ctrl>5']"
+
+### super + numberでアプリ切り替えを無効化
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
