2c17e885752fae27d77dc04ed7f41477bf686c85
inoue723 <omuni723@gmail.com>
2024-04-05 22:28:47 +0900

    clean

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,8 +2,6 @@ local wezterm = require 'wezterm'
 local act = wezterm.action
 local config = wezterm.config_builder()
  
-wezterm.font("Cica")
-
 config.window_background_opacity = 0.8
 config.color_scheme = 'OneDark (base16)'
 
@@ -12,9 +10,9 @@ config.keys = {}
 for i = 1, 8 do
     -- ALT + number to activate that tab
     table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'ALT',
-    action = act.ActivateTab(i - 1),
+      key = tostring(i),
+      mods = 'ALT',
+      action = act.ActivateTab(i - 1),
     })
 end
 
